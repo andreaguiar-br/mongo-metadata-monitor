b5f12fb3dd3bb2a0e0821e9df190ccca11179bd8
Andre LSA <andreaguiar_br@yahoo.com>
2021-03-27 16:41:19 -0300

    Teste com Prometheus

---
+++ test/testProm.py
@@ -0,0 +1,79 @@
+# importando bibliotecas para execução com Flask e disponibilizar monitoração prometheus com bb-flask-monitor
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+import traceback
+from flask_monitor import register_metrics, watch_dependencies, collect_dependency_time
+from flask import Flask
+import requests as req
+from prometheus_client import CollectorRegistry
+from time import time, sleep
+from random import random
+
+registry = CollectorRegistry()
+
+app = Flask(__name__)
+
+#nome da aplicação que aparece na monitoração
+app.config["APP_VERSION"] = "v0.0.1"
+
+## a handle function to return what is a http status code failure
+def is_error200(code):
+    code = str(code) if type(code) is int else code
+    return code.startswith("2")
+
+## register metrics
+# app is a flask instance and mandatory parameter.
+# buckets is the internavals for histogram parameter. buckets is a optional parameter
+# error_fn is a function to define what http status code is a error. By default errors are
+# 400 and 500 status code. error_fn is a option parameter
+register_metrics(app, buckets=[0.3, 0.6], error_fn=is_error200, registry=registry)
+
+
+# Plug metrics WSGI app to your main app with dispatcher
+dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app(registry=registry)})
+
+def check_db():
+    try:
+        response = req.get("http://localhost:5001/database")
+        app.logger.info(response)
+        return response.status_code < 400
+    except:
+        return 0  
+
+watch_dependencies("database", check_db, app=app, time_execution=500)
+
+# endpoint
+@app.route('/teste')
+def hello_teste():
+    return 'Hello, World!'
+
+# endpoint
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# endpoint
+@app.route('/database')
+def bd_running():
+    start = time()
+    # checks the database
+    sleep(random()/10)
+    # compute the elapsed time
+    elapsed = time() - start
+    # register the dependency time
+    collect_dependency_time(
+        app=app,
+        name='database',
+        rtype='http',
+        status=200,
+        is_error= 'False',
+        method='GET',
+        addr='external/database',
+        elapsed=elapsed
+    )
+    return 'I am a database working.'
+
+if __name__ == "__main__":
+    run_simple(hostname="localhost", port=5001, application=dispatcher)
+    # app.run()
